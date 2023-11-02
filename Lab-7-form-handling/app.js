281dd916bad77cea7e82c70504eab0d7cdedd8a5
annie-lopinto <143758564+annie-lopinto@users.noreply.github.com>
2023-11-02 16:19:22 -0400

    update

--- Lab-7-form-handling/app.js
+++ Lab-7-form-handling/app.js
@@ -1,33 +1,48 @@
 const express = require('express')
 
-const expressHandlebars = require('express-handlebars')
+//add the view engine 
+const expressHandlebars = require('express-handlebars') 
 
 const app = express()
 
+//configure our express app to use handlebars
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
 }))
 
 app.set('view engine','handlebars')
+//ends handlebar configuration
 
-const port = process.env.port | 3000
+const port = process.env.port || 3000
 
-app.get('/', (req,res)=> {
-    res.render('page')
+app.get('/',(req,res)=>{
+    res.render('page',{req})
 })
 
-app.use((req,res)=>{
-    res.status(500)
-    res.render('500')
+app.get('/mad',(req,res)=>{
+    const data = require('./data/mad-data.json')
+    res.render('madform',{data})
+})
+app.get('/madprocess',(req,res)=>{
+   res.render('madprocess',{req}) 
 })
 
-app.use((error,req,res,next) =>{
+//Error handling ->  app.use() basic express route 
+app.use((req,res) => {
+    res.status(404)
+    res.render('404')
+})
+
+//Server Error 500
+app.use((error,req,res,next) => {
     console.log(error.message)
     res.status(500)
-    res.render('500')
-})
+    res.render('500') 
+}) 
 
-app.listen(port,() =>{
-    console.log(`server started http://localhost:${port}`)
-    console.log('To close press Ctrl-C')
+// setup listener
+app.listen(port,()=>{
+    console.log(`Server started http://localhost:${port}`)
+    //console.log('Server starter http://localhost:'+port)
+    console.log('To close pres Ctrl-C')
 })
