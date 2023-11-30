f155de4b56b3a79d35c1f685a800569b7d7c078c
annie-lopinto <143758564+annie-lopinto@users.noreply.github.com>
2023-11-30 13:07:56 -0500

    help

--- Lab-7-form-handling/app.js
+++ Lab-7-form-handling/app.js
@@ -5,6 +5,12 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
+const handler = require('./lib/handler')
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
 //configure our express app to use handlebars
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
@@ -15,9 +21,13 @@ app.set('view engine','handlebars')
 
 const port = process.env.port || 3000
 
+const emails = []
 app.get('/',(req,res)=>{
-    res.render('page',{req})
+    var data = require('./data/home-data.json')
+    res.render('page',{data})
 })
+
+
 
 app.get('/mad',(req,res)=>{
     const data = require('./data/mad-data.json')
@@ -26,7 +36,22 @@ app.get('/mad',(req,res)=>{
 app.get('/madprocess',(req,res)=>{
    res.render('madprocess',{req}) 
 })
+//newsletter section
 
+
+app.get('/newsletter-signup', handler.newsletterSignup)
+
+app.get('/newsletter/list', handler.newsletterSignupList)
+
+app.get('/newsletter/details/:email',handler.newsletterUser)
+
+app.get('/newsletter/delete/:email',handler.newsletterUserDelete)
+
+app.post('/newsletter-signup/process', handler.newsletterSignupProcess)
+
+app.get('/newsletter/thankyou',(req,res) =>{
+    res.render('thankyou')
+})
 //Error handling ->  app.use() basic express route 
 app.use((req,res) => {
     res.status(404)
